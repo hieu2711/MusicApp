69307d187fc252b923014aa5fedcbf7e5426b932
Hieu <2051052042hieu@ou.edu.vn>
2023-12-20 13:44:58 +0700

    up

---
+++ Music Player in JavaScript/js/music-list.js
@@ -0,0 +1,50 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Bật Tình Yêu Lên",
+    artist: "Hòa Minzy ft Tăng Duy Tân",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Bo Xì Bo",
+    artist: "Hoàng Thùy Linh",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Em Đồng Ý",
+    artist: "Đức Phúc",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Nếu Lúc Đó",
+    artist: "TLinh",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "WaitingForYou",
+    artist: "Mono",
+    img: "music-5",
+    src: "music-5"
+  }
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
